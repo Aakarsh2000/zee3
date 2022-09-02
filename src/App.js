10c403cea36b1b24d84e8d2a087b5c2dcef79a97
Aakarsh2000 <saiaakarshp@gmail.com>
2022-09-02 17:39:34 +0530

    Added Navigation

--- src/App.js
+++ src/App.js
@@ -17,11 +17,24 @@ function App() {
       <h1>Hook</h1>
 
       <Routes>
-        <Route path ='/' element={<FirstHook/>}></Route>
-        <Route path ='/formshook' element={<FormsHook></FormsHook>}></Route>
+        <Route path ='/firsthook' element={<FirstHook/>}></Route>
+        <Route path ='/formshook' element={<FormsHook/>}></Route>
+        <Route path ='/arrayhook' element={<ArrayHook/>}></Route>
+        <Route path ='/demoue' element={<DemoUE/>}></Route>
+        <Route path ='/demouehooks' element={<DemoUEHooks/>}></Route>
+        <Route 
+          path ='/demolastnode' 
+          element={<UserContext.Provider value = "Sai">
+        <DemoLastNode/>
+      </UserContext.Provider>}></Route>
       </Routes>
 
-
+      <Link to="/firsthook"><h2>First Hook page</h2></Link>
+      <Link to="/formshook"><h2>Forms Hook page</h2></Link>
+      <Link to="/arrayhook"><h2>Array Hook page</h2></Link>
+      <Link to="/demoue"><h2>Demo UE page</h2></Link>
+      <Link to="/demouehooks"><h2>Demo UE Hooks page</h2></Link>
+      <Link to="/demolastnode"><h2>Demo Last Node page</h2></Link>
 
 
 
--- src/components/FirstHook.js
+++ src/components/FirstHook.js
@@ -1,5 +1,4 @@
 import React, {useState} from 'react'
-import {Routes,Route, Link} from 'react-router-dom';
 
 export default function FirstHook() {
     const initialCount=0
@@ -12,12 +11,12 @@ export default function FirstHook() {
     }
   return (
     <div>
-        <Link to="/formshook"><h2>My count is {count}</h2></Link>
+        <h2>My count is {count}</h2>
       <button onClick={() => setCount(count+1)}>Counter INC</button>
       <button onClick={() => setCount(count-1)}>Counter DEC</button>
       <button onClick={() => setCount(initialCount)}>Counter RESET</button>
-
       <button onClick={incrementbyfive}>Counter INC by 5</button>
+
     </div>
   )
 }
