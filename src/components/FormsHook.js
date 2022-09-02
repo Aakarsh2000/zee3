80c932b6086afa3e6cf452438149d1dcf5d5379b
Aakarsh2000 <saiaakarshp@gmail.com>
2022-09-02 12:42:59 +0530

    Added submit

--- src/components/FormsHook.js
+++ src/components/FormsHook.js
@@ -3,8 +3,12 @@ import React,{useState} from 'react'
 function FormsHook() {
 
     const [name, setName] = useState({firstname:"",lastname:""})
+    const onHandleSubmit = (e) =>{
+      alert(name.firstname +" "+ name.lastname)
+    }
   return (
     <div>
+      <form onSubmit={onHandleSubmit}>
         <input 
             type ="text" 
             value={name.firstname} 
@@ -16,8 +20,12 @@ function FormsHook() {
             onChange={(e) => setName({...name,lastname:e.target.value})}>
         </input>
 
+        <button type='submit'> SUBMIT</button>
+      </form>
         <h2>Your First Name is {name.firstname}</h2>
         <h2>Your Last Name is {name.lastname}</h2>
+
+
     </div>
   )
 }
